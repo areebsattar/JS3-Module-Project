62b74d16d8386cb1aa5768d7a6dfa23e479ad3b9
areebsattar <areebsattar1@gmail.com>
2024-02-03 15:20:44 +0000

    updated function to display required information

--- script.js
+++ script.js
@@ -6,7 +6,22 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for (const episode of episodeList) {
+    const card = document.getElementById("show-card").content.cloneNode(true);
+    const title = card.querySelector("#show-title");
+    title.textContent = episode.name;
+    title.setAttribute("href", episode.url);
+    card.querySelector("#episode-code").textContent = episodeCode(episode.season, episode.number);
+    card.querySelector("#episode-img").src = episode.image.medium;
+    card.querySelector("#episode-summary").innerHTML = episode.summary;
+    rootElem.append(card);
+  }
 }
 
+function episodeCode(season, number) {
+  let s = season < 10 ? `0${season}` : season;
+  let num = number < 10 ? `0${number}` : number;
+  let code = `S${s}E${num}`;
+  return code;
+}
 window.onload = setup;
